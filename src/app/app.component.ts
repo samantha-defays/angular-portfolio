49cf1495f22a09ad2c34e661ccdc704a2b399540
Samantha Defays <samantha.defays@gmail.com>
2024-12-27 21:31:34 +0100

    Retrograde en TS 5.6.2, suppression du routing pour la présentation en SPA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { FormationsComponent } from './formations/formations.component';
+import { ExperiencesComponent } from './experiences/experiences.component';
+import { CompetencesComponent } from './competences/competences.component';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, RouterLink, RouterLinkActive],
+    imports: [HomeComponent, FormationsComponent, ExperiencesComponent, CompetencesComponent],
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,9 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
 
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes)
   ]
 }).catch(err => console.error(err));
