16e5b1d46a9aa712159f12a6378b005af11e368c
Samantha Defays <samantha.defays@gmail.com>
2024-12-28 13:12:03 +0100

    Mise en place d'un snap scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
 import { FormationsComponent } from './formations/formations.component';
 import { ExperiencesComponent } from './experiences/experiences.component';
 import { CompetencesComponent } from './competences/competences.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
     selector: 'app-root',
-    imports: [HomeComponent, FormationsComponent, ExperiencesComponent, CompetencesComponent],
+    imports: [HomeComponent, ProfileComponent, FormationsComponent, ExperiencesComponent, CompetencesComponent, ContactComponent],
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
